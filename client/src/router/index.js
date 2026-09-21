429d4c14160d5e2df5e322372f2a7d58907a24ca
y4erhs <shreya.g.2025@computing.smu.edu.sg>
2026-09-21 14:31:23 +0800

    feat: display dropdown of categories

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import AboutView from "../views/AboutView.vue";
 import MenuView from "../views/MenuView.vue";
 import CartView from "../views/CartView.vue";
-
+// importing from views instead of components 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- client/src/views/MenuView.vue
+++ client/src/views/MenuView.vue
@@ -14,7 +14,19 @@ const cartItems = ref([]);
 const STORAGE_KEY = "cart"
 
 // TODO: Fetch categories when the component is created
+onMounted(async () => {
+    let url = "http://127.0.0.1:3000/categories"
 
+    try {
+        let response = await axios.get(url)
+        
+        console.log(response.data) // check if you can see it in the console
+
+        categories.value = response.data
+    } catch(e) {
+        console.log(e.message)
+    }
+})
 
 // TODO: Fetch items for the currently selected category
 async function getItems() {
@@ -50,7 +62,8 @@ function doAddToCart(itemsToAdd) {
     <!-- TODO: Category selection dropdown -->
     <label for="categories">Categories</label>
     <select class="form-control" id="categories" >
-        <option> category </option>
+        <option v-for="category in categories"> {{ category }} </option>
+        
     </select>
     <br>
 
